9781b106cead9e53a9f97b51e1956fbf7c5de326
Abi-390 <dekaabinash07@gmail.com>
2026-04-28 11:33:08 +0530

    Succesfully tested login service in postman with demo controller

--- app/controllers/AuthController.scala
+++ app/controllers/AuthController.scala
@@ -4,7 +4,7 @@ import javax.inject._
 import play.api.mvc._
 import play.api.libs.json._
 import services.AuthService
-import dtos.CreateUserRequest
+import dtos.{CreateUserRequest, LoginRequest}
 
 @Singleton
 class AuthController @Inject()(val controllerComponents: ControllerComponents, authService: AuthService)extends BaseController {
@@ -23,5 +23,28 @@ class AuthController @Inject()(val controllerComponents: ControllerComponents, a
     })
   }
 
+  def login: Action[JsValue] = Action(parse.json){request =>
+    request.body.validate[LoginRequest].fold(errors =>{
+      BadRequest(Json.obj("status" -> "Error","Message"->JsError.toJson(errors)))
+    }, loginReq =>{
+      try {
+        // authService.login returns Option[AuthResponse]
+        authService.login(loginReq).map { response =>
+          // Case: Some(AuthResponse) -> Password was correct
+          println("Logged in successfully")
+          Ok(Json.toJson(response))
+
+
+        }.getOrElse {
+          // Case: None -> User not found OR password wrong
+          println("Failed to login , invalid email or password")
+          Unauthorized(Json.obj("message" -> "Invalid email or password"))
+        }
+      } catch {
+        case e: Exception =>
+          InternalServerError(Json.obj("message" -> s"Login failed: ${e.getMessage}"))
+      }
+    })
+  }
 
 }
